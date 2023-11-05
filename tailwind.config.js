f3bcfcdf745bc145242d18129c9118373569b4cc
satishnadipalli <satishnadipalli1@gmail.com>
2023-11-05 16:28:32 +0530

    added Cart and Login Feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height :{
+        '50' : '150px',
+        '60' : '280px',
+        '100' : '250px'
+      },
+      width : {
+        '50': '150px',
+
+        '100': '300px'
+      }
+    },
   },
   plugins: [],
 };
